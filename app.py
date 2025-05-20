9e81cc2575a12c2a5cd492b6263b4dfbd6b09b1f
mtwinarske <mtwin@uw.edu>
2025-05-20 10:58:45 -0700

    update app.py to have 0.0.0.0. port for render deploy

--- app.py
+++ app.py
@@ -40,5 +40,7 @@ def save_shelter():
 def data_files(filename):
     return send_from_directory("data", filename)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
