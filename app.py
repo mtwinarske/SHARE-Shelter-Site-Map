a0b76f1c962682f861696e5257bf01b6c2d40344
mtwinarske <mtwin@uw.edu>
2025-05-20 11:00:34 -0700

    imported os to app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import json
 from pathlib import Path
+import os
 
 app = Flask(__name__, static_folder="static")
 CORS(app)
@@ -43,4 +44,3 @@ def data_files(filename):
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-    
